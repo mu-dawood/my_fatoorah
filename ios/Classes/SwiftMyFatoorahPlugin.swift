4531da9d4bc2022d458a3a619dfba505610cea05
Mohamed Dawood <dawood@Mohameds-Mac.local>
2019-11-18 17:33:52 -0800

    init ios

--- ios/Classes/SwiftMyFatoorahPlugin.swift
+++ ios/Classes/SwiftMyFatoorahPlugin.swift
@@ -1,7 +1,14 @@
 import Flutter
 import UIKit
+import MFSDK
 
-public class SwiftMyFatoorahPlugin: NSObject, FlutterPlugin {
+public class SwiftMyFatoorahPlugin: NSObject, FlutterPlugin , UINavigationControllerDelegate,MFInvoiceCreateStatusDelegate {
+    var results : FlutterResult!
+    var navigationController: UINavigationController!
+
+    override init(){
+        super.init()
+    }
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "my_fatoorah", binaryMessenger: registrar.messenger())
     let instance = SwiftMyFatoorahPlugin()
@@ -9,6 +16,29 @@ public class SwiftMyFatoorahPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    if(call.method.elementsEqual("config")){
+        config(call,result);
+    }
+    else if(call.method.elementsEqual("initiatePayment")){
+        initiatePayment(call,result);
+    }
+    else if(call.method.elementsEqual("executePayment")){
+        executePayment(call,result);
+    }
+  }
+
+  public func config(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+         do {
+            
+        } catch {
+          
+            print(error.localizedDescription)
+        }
+  }
+  public func initiatePayment(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    result("iOS " + UIDevice.current.systemVersion)
+  }
+  public func executePayment(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
